b03e90eb7178dab0423ff1dfa74b80413cdc7592
Erival Junior <sanjunior@gmail.com>
2022-10-13 21:55:26 -0300

    feat: adiciona serviço de rota pra post

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,54 +1,74 @@
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
-const apiUrl = environment.apiUrl+'api/v1';
+const apiUrl = environment.apiUrl + 'api/v1';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class ApiService {
+  constructor(
+    private httpClient: HttpClient,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
-  constructor(private httpClient: HttpClient,private http: HttpClient,
-    private router: Router) { }
+  /**
+   * Generic post data to API
+   * @param route
+   * @param data
+   */
+  public postData = (route: string, data: object) => {
+    const url = `${apiUrl}/${route}`;
+    return this.httpClient.post(url, data).pipe(
+      tap(console.log),
+      catchError((error) => {
+        console.error(error);
+        return error;
+      })
+    );
+  };
 
-  /*public getData1 = (route: string) => {
-    return this.http.get(this.createCompleteRoute(route, apiUrl));
-  }*/
-
+  /**
+   * Generic parser data
+   * @param route
+   */
   public getData = (route: string) => {
-    return this.httpClient.get<Object[]>(apiUrl+"/"+route).pipe(
+    const url = `${apiUrl}/${route}`;
+    return this.httpClient.get<object[]>(url).pipe(
       tap((resposta) => {
-        if(!resposta) {
+        if (!resposta) {
           console.error(resposta);
-          return;}
-      }), catchError(error => {
-        console.log('Erro ao listar',typeof(error))
-        throw(error);
-      }));
-  }
-
+        }
+      }),
+      catchError((error) => {
+        console.log('Erro ao listar', typeof error);
+        throw error;
+      })
+    );
+  };
 
   public delete = (route: string) => {
-    return this.httpClient.delete(apiUrl+"/"+route).pipe(
+    return this.httpClient.delete(apiUrl + '/' + route).pipe(
       tap((resposta) => {
-        if(!resposta) {
+        if (!resposta) {
           console.error(resposta);
-          return;}          
-      }), catchError(error => {
-        console.log('Erro ao delete',typeof(error))
-        throw(error);
-      }));
-  }
+          return;
+        }
+      }),
+      catchError((error) => {
+        console.log('Erro ao delete', typeof error);
+        throw error;
+      })
+    );
+  };
   /*public create1 = (route: string,body:Object ) => {
     return this.http.post(this.createCompleteRoute(route, apiUrl), body, this.generateHeaders());
   }*/
-/*
+  /*
   public create = (route: string,body:Object ) => {
     return this.httpClient.get<Object>( apiUrl+route).pipe(
       tap((resposta) => {
@@ -61,7 +81,7 @@ export class ApiService {
       }));
     }
   }
-  
+
   public update = (route: string, body:Object) => {
    return this.httpClient.put<Object>( apiUrl+route,body).pipe(
       tap((resposta) => {
@@ -73,20 +93,18 @@ export class ApiService {
         throw(error);
       }));
     }*/
-  
- /*
-  */
+
+  /*
+   */
   /*public update1 = (route: string, body:Object) => {
     return this.http.put(this.createCompleteRoute(route, apiUrl), body, this.generateHeaders());
   }
- 
+
   public delete1 = (route: string) => {
     return this.http.delete(this.createCompleteRoute(route, apiUrl));
   }*/
- 
-  
- 
- /* private generateHeaders = () => {
+
+  /* private generateHeaders = () => {
     return {
       headers: new HttpHeaders({'Content-Type': 'application/json'})
     }
